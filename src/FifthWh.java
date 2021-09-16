70994cd10592272611008becab2c501a3713b2b7
Atieh Rahbari Manesh <61627824+Atiehrm@users.noreply.github.com>
2021-09-16 15:02:28 +0430

    Update FifthWh.java

--- src/FifthWh.java
+++ src/FifthWh.java
@@ -10,5 +10,6 @@ public class FifthWh {
         System.out.println("first change");
         System.out.println("2 change dev");
         System.out.println("2 change dev");
+         System.out.println("salam");
     }
 }
