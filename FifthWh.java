a9822ec9d993512bafc686084fed8727ed343013
Atieh <atieh.rahbari@gmail.com>
2021-09-16 11:53:05 +0330

    add java file

---
+++ FifthWh.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FifthWh {
+    public static void main(String[] args) {
+      isVowel();
+    }
+
+    public static void isVowel() {
+        System.out.println("first change");
+        System.out.println("2 change dev");
+        System.out.println("2 change dev");
+    }
+}
